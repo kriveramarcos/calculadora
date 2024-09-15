bb05ea9ac72489b5636c0af599975b20a1945342
Kevin Rivera <kevin.riveram@unmsm.edu.pe>
2024-09-15 18:17:08 -0500

    menu mas completo

--- menu.py
+++ menu.py
@@ -1,7 +1,29 @@
-print("MENU CALCULADORA");
+def main():
 
-print("1. Suma");
-print("2. Resta");
-print("3. Division");
-print("4. Multipplicacion");
-print("0. SALIR")
+    print("MENU CALCULADORA");
+
+def mostrar_menu():
+
+    print("1. Suma");
+    print("2. Resta");
+    print("3. Division");
+    print("4. Multipplicacion");
+    print("0. SALIR")
+
+    
+    mostrar_menu()
+
+        opcion = input("Ingresa el número de la operación que deseas realizar: ")
+        
+        if opcion == '0':
+            print("Saliendo del programa...")
+            break
+        
+        if opcion in ['1', '2', '3', '4']:
+            try:
+                a = float(input("Ingresa el primer número: "))
+                b = float(input("Ingresa el segundo número: "))
+                
+            except ValueError:
+                print("Por favor ingresa números válidos.")
+                continue
